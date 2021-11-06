097fb9c50f342bb5e750de49979bdb2ba7e20e9b
Maryna1704 <74004216+Maryna1704@users.noreply.github.com>
2021-11-06 16:45:56 +0200

    hw 5 done

--- middelwares/index.js
+++ middelwares/index.js
@@ -1,9 +1,11 @@
 const controllerWrapper = require('./controllerWrapper');
 const validator = require('./validator');
 const authenticate = require('./authenticate');
+const upload = require('./upload')
 
 module.exports = {
   controllerWrapper,
   validation,
-  authenticate
-};
+  authenticate,
+  upload
+}
